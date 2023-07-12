f675cfa0e501e9672d9b346baea898f8dbedfd74
Artemius <cpt.morgan0510@gmail.com>
2023-07-12 13:43:36 +0300

    First commit

---
+++ src/test/java/Task/TaskTest.java
@@ -0,0 +1,74 @@
+package Task;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    @Test
+    public void testSimpleTaskIfMatches() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Позвонить");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testSimpleTaskIfNotMatches() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Написать");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testEpicIfMatches() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Молоко");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMeetingIfMatchesInTopic() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        boolean actual = meeting.matches("приложения");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMeetingIfMatchesInProject() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        boolean actual = meeting.matches("НетоБанка");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMeetingNoMatches() {
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        boolean actual = meeting.matches("ужина");
+        Assertions.assertFalse(actual);
+    }
+}
